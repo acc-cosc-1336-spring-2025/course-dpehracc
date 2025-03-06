1160049a5d836d7c78eae64b858ace96b4d6b0c2
dpehracc <deborah.pehr@g.austincc.edu>
2025-03-06 22:44:21 +0000

    HW2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,16 @@
-#
+#create function to accept two parameters with names option, total and returns the ratio
+def get_options_ratio(options, total):
+    return options / total
+
+#create a function named get_faculty_rating that accepts the ratio according to the table
+def get_faculty_rating(ratio):
+    if 0.9 <= ratio < 1:
+        return "Excellent"
+    elif ratio > 0.8:
+        return "Very Good"
+    elif ratio > 0.7:
+        return "Good"
+    elif ratio > 0.6:
+        return "Needs Improvement"
+    else:
+        return "Unacceptable"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,12 @@
-#
+import decisions#
+
+#Gather options and total input
+options = int(input("What is the number of options?"))
+total = int(input("What is the total?"))
+
+#Calculate the ratio
+result = decisions.get_options_ratio(options, total)
+
+#Display the faculty rating
+rating = decisions.get_faculty_rating(result)
+print("Faculty Rating: " + rating)
