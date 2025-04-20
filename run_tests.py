4343f95e336f38e825345c7430e98eec4559016f
dpehracc <deborah.pehr@g.austincc.edu>
2025-04-20 02:47:20 +0000

    3

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,5 @@
 import unittest
+from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
 
-from tests.homework.g_lists_and_tuples import tests_lists_and_tuples
-
-suite = unittest.TestLoader().loadTestsFromModule(tests_lists_and_tuples)
-unittest.TextTestRunner().run(suite)
+suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
+unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,30 @@
 #
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
+import unittest
+from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory_widget
+
+class Test_Config(unittest.TestCase):
+
+    def test_add_inventory(self):
+        inventory = {}
+        add_inventory(inventory, "Widget1", 10)
+        self.assertEqual(inventory["Widget1"], 10)
+
+        add_inventory(inventory, "Widget1", 25)
+        self.assertEqual(inventory["Widget1"], 35)
+
+        add_inventory(inventory, "Widget1", -10)
+        self.assertEqual(inventory["Widget1"], 25)
+
+    def test_remove_inventory_widget(self):
+        inventory = {"Widget1": 10, "Widget2": 20}
+        result = remove_inventory_widget(inventory, "Widget1")
+        self.assertEqual(result, "Record deleted")
+        self.assertEqual(len(inventory), 1)
+        self.assertIn("Widget2", inventory)
+
+        result_not_found = remove_inventory_widget(inventory, "Nonexistent")
+        self.assertEqual(result_not_found, "Item not found")
